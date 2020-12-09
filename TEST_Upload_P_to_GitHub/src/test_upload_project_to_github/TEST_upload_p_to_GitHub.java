f0e903a36976202e6fafcc92da657acb597854f6
Julio_Caser <julio6600c3oh28@hotmail.com>
2020-12-09 19:09:56 +0100

    Complete TEST_upload

--- TEST_Upload_P_to_GitHub/src/test_upload_project_to_github/TEST_upload_p_to_GitHub.java
+++ TEST_Upload_P_to_GitHub/src/test_upload_project_to_github/TEST_upload_p_to_GitHub.java
@@ -1,9 +1,14 @@
 package test_upload_project_to_github;
 
 public class TEST_upload_p_to_GitHub {
-
+    
+	/**
+	 * 
+	 * @param args This is command line.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hej Jonas this is only TEST how I upload a Project to GitHub");
+		
 
 	}
 
